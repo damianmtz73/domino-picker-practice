117355941941feb16f6712a0df31f9990e652cbb
damian martinez <damianmtz9@gmail.com>
2021-09-01 15:55:35 -0400

    added code

--- main.js
+++ main.js
@@ -5,14 +5,31 @@ function getRandomNumber(min, max){
 
 
 
-let dominoList= document.querySelectorAll(".domino-list > li")
+// let dominoList= document.querySelectorAll(".domino-list > li")
+
+
+let dominoCharacters='🁣🁤🁥🁦🁧🁨🁩🁪🁫🁬🁭🁮🁯🁰🁱🁲🁳🁴🁵🁶🁷🁸🁹🁺🁻🁼🁽🁾🁿🂀🂁🂂🂃🂄🂅🂆🂇🂈🂉🂊🂋🂌🂍🂎🂏🂐🂑🂒🂓'
+let dominoList= Array.from(dominoCharacters)
 
 let firstIndex= 0
 let lastIndex= dominoList.length-1
-
 let randomIndex= getRandomNumber(firstIndex, lastIndex) 
+let randomDominoCharacter= dominoList[randomIndex]
 
-let randomDominoElement= dominoList[randomIndex]
+for(let index = 0; index < dominoList.length; index+=1){
+    let currentDomino= dominoList[index]
+    
+    let dominoElement= document.createElement("div")
+dominoElement.append(currentDomino)
+dominoElement.classList.add("domino") 
 
-randomDominoElement.classList.add("selected")
+let mainElement= document.querySelector("main")
+mainElement.append(dominoElement )
+
+if (currentDomino === randomDominoCharacter){
+    dominoElement.classList.add("selected")
+}
+
+}
+
  
